fd736172732406366c98bd0f92694064d96eb1b1
Muhammad Agung Satrio <agung.satrio@bukalapak.com>
2021-04-10 11:04:20 +0700

    create custom view player

---
+++ app/src/main/java/com/satryaway/bcgdvtest/MediaPlayerView.kt
@@ -0,0 +1,30 @@
+package com.satryaway.bcgdvtest
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.RelativeLayout
+
+class MediaPlayerView : RelativeLayout {
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+    ) : super(context, attrs, defStyleAttr)
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        inflate(context, R.layout.bottom_player_view, this)
+    }
+}
